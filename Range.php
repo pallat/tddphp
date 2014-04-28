c68010845cd199795d16894f95d0e0c86853ab52
roofimon <roofimon@gmail.com>
2014-04-28 22:58:35 +0700

    Refactor

--- Range.php
+++ Range.php
@@ -3,29 +3,42 @@
 function rangeZeroToFive($range) {
   $rangeOutput ="{";
 
-  $rangeInput = explode(",",substr($range,1,3));
-
-
-    if($range[0] == "[") {
-	$startLoop = $rangeInput[0];
-    } else {
-	$startLoop = $rangeInput[0] + 1;
-    }
-
-    if ($range[strlen($range)-1] == "]"){
-	$endLoop = $rangeInput[1];
-    }else{
-	$endLoop = $rangeInput[1] -1;
-    }
-
-    if($endLoop >= $startLoop){
-      for($index=$startLoop;$index<=$endLoop;$index++){
-	$rangeMemberArray[$index] = $index;
-      }
-    
-      $rangeOutput .= implode($rangeMemberArray,",");
-    }
+  $startLoop = setStartLoop($range);
+  $endLoop = setEndLoop($range);
+  $rangeOutput = $rangeOutput. rangeOutput($startLoop, $endLoop);
 
   $rangeOutput .="}";
+  return $rangeOutput;
+}
+
+function setEndLoop($range) {
+  $rangeInput = explode(",",substr($range,1,3));
+  if ($range[strlen($range)-1] == "]"){
+    $endLoop = $rangeInput[1];
+  }else{
+    $endLoop = $rangeInput[1] -1;
+  }
+  return $endLoop;
+}
+
+function setStartLoop($range) {
+  $rangeInput = explode(",",substr($range,1,3));
+  if($range[0] == "[") {
+    $startLoop = $rangeInput[0];
+  } else {
+    $startLoop = $rangeInput[0] + 1;
+  }
+  return $startLoop;
+}
+
+function rangeOutput($startLoop, $endLoop) {
+  $rangeOutput="";
+  if($endLoop >= $startLoop){
+    for($index=$startLoop;$index<=$endLoop;$index++){
+      $rangeMemberArray[$index] = $index;
+    }
+
+    $rangeOutput .= implode($rangeMemberArray,",");
+  }
   return $rangeOutput;
 }
